e8efef9d88906736c5d63bb8301510d1213b367e
keshavsoft <nknnkumar@yahoo.com>
2024-10-04 14:50:56 +0530

    new vite working

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,43 @@
-import { resolve } from 'path'
+import { defineConfig, normalizePath } from 'vite';
 
-export default {
-    root: resolve(__dirname, 'src'),
-    build: {
-        outDir: '../dist'
+import path, { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+import { StartFunc as StartFuncGetFiles } from "./KCode/getFiles.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const SrcFolder = "src";
+
+const FrontEndDistFolder = "publicDir/TableShow";
+
+let files = StartFuncGetFiles({ inSrcPath: SrcFolder });
+
+export default defineConfig((env) => ({
+    publicDir: 'static',
+    base: './',
+    root: resolve(__dirname, SrcFolder),
+    plugins: [
+    ],
+    resolve: {
+        alias: {
+            '@': normalizePath(resolve(__dirname, 'src')),
+            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+            '~bootstrap-icons': resolve(__dirname, 'node_modules/bootstrap-icons'),
+            '~perfect-scrollbar': resolve(__dirname, 'node_modules/perfect-scrollbar'),
+            '~@fontsource': resolve(__dirname, 'node_modules/@fontsource'),
+        }
     },
-    server: {
-        port: 8080
+    build: {
+        emptyOutDir: false,
+        manifest: true,
+        target: "chrome58",
+        outDir: resolve(__dirname, FrontEndDistFolder),
+        rollupOptions: {
+            input: files,
+            output: {
+            }
+        },
     }
-}
+}));
