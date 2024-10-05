6700c44290234c7e1cbd160e9cd20bed4e11209d
keshavsoft <nknnkumar@yahoo.com>
2024-10-05 12:38:43 +0530

    send working

--- prince/index.js
+++ prince/index.js
@@ -4,15 +4,30 @@ let StartFunc = () => {
     jVarLocalSendButtonId.addEventListener("click", ClickFunc);
 };
 
-let ClickFunc = () => {
+let jFLocalForTemplate = () => {
     console.log("button clicked");
 
     let jVarLocalMessageContainerId = document.getElementById('MessageContainerId');
+    let jVarLocalMessage = jFLocalMessageInputId();
 
     let jVarLocalTemplateFromHtml = document.getElementById('TemplateFromSendId');
     let clone = jVarLocalTemplateFromHtml.content.cloneNode('true');
+    clone.querySelector("p").innerHTML = jVarLocalMessage;
 
     jVarLocalMessageContainerId.appendChild(clone)
+};
+
+let jFLocalMessageInputId = () => {
+    let jVarLocalMessageInputId = 'MessageInputId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalMessageInputId);
+
+    if (jVarLocalHtmlId === null === false) {
+        return jVarLocalHtmlId.value.trim();
+    };
+};
+
+let ClickFunc = () => {
+    jFLocalForTemplate();
 };
 
 StartFunc();
